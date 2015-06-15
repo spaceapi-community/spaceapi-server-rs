3cfc1e90ecafbc377ce63e1f0c6fdc12e7b2b494
Raphael Nestler <raphael.nestler@gmail.com>
2015-06-15 22:13:50 +0200

    DataStore: Add Send trait
    To be able to share DataStore between threads it must implement Send.

--- src/datastore.rs
+++ src/datastore.rs
@@ -1,7 +1,7 @@
 extern crate redis;
 
 /// A DataStore needs to implement store and retrieve methods.
-pub trait DataStore {
+pub trait DataStore : Send {
     fn store(&self, key: &str, value: &str) -> Result<(), DataStoreError>;
     fn retrieve(&self, key: &str) -> Result<String, DataStoreError>;
     fn delete(&self, key: &str) -> Result<(), DataStoreError>;
