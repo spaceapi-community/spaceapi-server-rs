80eac1bd6bc7da6200de2328386b9516c1ecfab2
Danilo Bargen <mail@dbrgn.ch>
2015-11-19 01:58:38 +0100

    Remove the datastore module
    This does various things:
    
    - Remove the datastore module and all datastore implementations
    - Replace the datastore instance in the `SpaceapiServer` with a
      Redis `ConnectionInfo` object.
    - This object is passed around to the handlers instead of the datastore
      Arc Mutex.
    - Previously the datastore access was inconsistent: Sometimes the
      sensor specs fetched their own values, but sometimes we updated the
      values in the datastore directly, without going through the sensor
      spec. In the current codebase, the sensor specs are responsable for
      accessing Redis to fetch or update sensor values.

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -5,21 +5,22 @@ use std::sync::{Arc, Mutex};
 
 use iron::Iron;
 use router::Router;
+use redis::{IntoConnectionInfo, ConnectionInfo};
+
+mod handlers;
 
 use ::api;
 use ::api::SensorTemplate;
 
-use ::datastore;
 use ::sensors;
 use ::modifiers;
-
-mod handlers;
+use ::errors;
 
 
 /// A Space API server instance.
 ///
 /// You can create a new instance using the ``new`` constructor method by
-/// passing it the host, the port, the ``Status`` object and a datastore.
+/// passing it the host, the port, the ``Status`` object and a redis connection info object.
 ///
 /// The ``SpaceapiServer`` includes a web server through
 /// [Hyper](http://hyper.rs/hyper/hyper/server/index.html). Simply call the ``serve`` method.
@@ -27,33 +28,34 @@ pub struct SpaceapiServer {
     host: Ipv4Addr,
     port: u16,
     status: api::Status,
-    datastore: datastore::SafeDataStore,
+    redis_connection_info: ConnectionInfo,
     sensor_specs: sensors::SafeSensorSpecs,
     status_modifiers: Vec<Box<modifiers::StatusModifier>>,
 }
 
 impl SpaceapiServer {
 
-    pub fn new(host: Ipv4Addr,
-               port: u16,
-               status: api::Status,
-               datastore: datastore::SafeDataStore,
-               status_modifiers: Vec<Box<modifiers::StatusModifier>>)
-               -> SpaceapiServer {
-        SpaceapiServer {
+    pub fn new<T>(host: Ipv4Addr, port: u16,
+                  status: api::Status,
+                  redis_connection_info: T,
+                  status_modifiers: Vec<Box<modifiers::StatusModifier>>)
+                  -> Result<SpaceapiServer, errors::SpaceapiServerError>
+                  where T: IntoConnectionInfo {
+
+        Ok(SpaceapiServer {
             host: host,
             port: port,
             status: status,
-            datastore: datastore,
+            redis_connection_info: try!(redis_connection_info.into_connection_info()),
             sensor_specs: Arc::new(Mutex::new(vec![])),
             status_modifiers: status_modifiers,
-        }
+        })
     }
 
     fn route(self) -> Router {
         router!(
-            get "/" => handlers::ReadHandler::new(self.status.clone(), self.datastore.clone(), self.sensor_specs.clone(), self.status_modifiers),
-            put "/sensors/:sensor/" => handlers::UpdateHandler::new(self.datastore.clone(), self.sensor_specs.clone())
+            get "/" => handlers::ReadHandler::new(self.status.clone(), self.redis_connection_info.clone(), self.sensor_specs.clone(), self.status_modifiers),
+            put "/sensors/:sensor/" => handlers::UpdateHandler::new(self.redis_connection_info.clone(), self.sensor_specs.clone())
         )
     }
 
@@ -73,7 +75,7 @@ impl SpaceapiServer {
     /// Register a new sensor.
     ///
     /// The first argument is a ``api::SensorTemplate`` instance containing all static data.
-    /// The second argument specifies how to get the actual sensor value from the datastore.
+    /// The second argument specifies how to get the actual sensor value from Redis.
     /// And the third argument specifies the data type of the value.
     pub fn register_sensor(&mut self, template: Box<api::SensorTemplate>, data_key: String) {
         let sensor_specs_ref = self.sensor_specs.clone();
