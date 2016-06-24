670cac785021a2c613974faf91dbac6558ef9b2d
Danilo Bargen <mail@dbrgn.ch>
2016-06-24 16:17:08 +0200

    Add example that uses sensors (#38)

---
+++ examples/with_sensors.rs
@@ -0,0 +1,53 @@
+extern crate spaceapi_server;
+extern crate env_logger;
+
+use spaceapi_server::SpaceapiServer;
+use spaceapi_server::api;
+use spaceapi_server::api::Optional::{Value, Absent};
+use spaceapi_server::api::sensors::PeopleNowPresentSensorTemplate;
+use spaceapi_server::modifiers::{StatusModifier, StateFromPeopleNowPresent};
+
+
+fn main() {
+    env_logger::init().unwrap();
+
+    // Create new minimal Status instance
+    let status = api::Status::new(
+        "coredump",
+        "https://www.coredump.ch/logo.png",
+        "https://www.coredump.ch/",
+        api::Location {
+            address: Value("Spinnereistrasse 2, 8640 Rapperswil, Switzerland".into()),
+            lat: 47.22936,
+            lon: 8.82949,
+        },
+        api::Contact {
+            irc: Value("irc://freenode.net/#coredump".into()),
+            twitter: Value("@coredump_ch".into()),
+            foursquare: Absent,
+            email: Absent,
+        },
+        vec![
+            "email".into(),
+            "twitter".into(),
+        ],
+    );
+
+    // Set up server
+    let listen = "127.0.0.1:8000";
+    let redis = "redis://127.0.0.1/";
+    let modifiers: Vec<Box<StatusModifier + 'static>> = vec![Box::new(StateFromPeopleNowPresent)];
+    let mut server = SpaceapiServer::new(listen, status, redis, modifiers)
+                                    .expect("Could not initialize server");
+
+    // Register sensors
+    server.register_sensor(Box::new(PeopleNowPresentSensorTemplate {
+        location: Value("Hackerspace".into()),
+        name: Absent,
+        description: Absent,
+        names: Absent,
+    }), "people_now_present".into());
+
+    // Serve!
+    server.serve().expect("Could not start the server");
+}
